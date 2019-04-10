34a0d161c82def3edf7ee56bf87929a0d655511f
noelz8 <48070788+noelz8@users.noreply.github.com>
2019-04-10 14:24:09 -0600

    Add files via upload

--- ordenaar filas.py
+++ ordenaar filas.py
@@ -10,7 +10,7 @@ class ordenar(object):
     def orden_aux(self,lista,indice):
         if indice==len(lista)-1:
             return lista
-        else: return sefl.ordernar2_aux(lista,0)+orden_aux(lista,indice+1)
+        else: return self.orden_aux(sefl.orden_aux2(lista,0),indice+1)
 
     def orden_aux2(self,lista,indice):
         if indice==len(lista)-1:
@@ -19,7 +19,20 @@ class ordenar(object):
             aux= lista[indice]
             lista[indice]=lista[indice+1]
             lista[indice+1]=aux
-            return orden2_aux(lista,indice+1)
-        else: return orden2_aux(lista,indice+1)
+            return self.orden_aux2(lista,indice+1)
+        else: return self.orden_aux2(lista,indice+1)
         
+    ### se usa el ordenamiento burbuja
+        #alternativo
+        def orden2(self,lista):
+        if isinstance (lista,list) and lista !=[]:
+            return self.orden2_aux(lista,0,0)
+        else: return "Error"
+
+    def orden2_aux(self,lista,indice1,indice2):
+        if indice2==len(lista)-1:
+            return lista
+        elif indice1==len(lista)-1:
+            return orden2_aux(lista,0,indice2+1)
+        elif
         
