8cd14a9e3d2f6708cec43bf98835dfbd4caef038
noelz8 <48070788+noelz8@users.noreply.github.com>
2019-04-26 16:38:59 -0600

    Add files via upload

---
+++ prueba multi de matrices.py
@@ -0,0 +1,17 @@
+#Multiplicar matrices
+class multi(object):
+    def __init__(self):
+        pass
+    def multi_matriz(self,n,m):
+        if isinstance (n,list) and n!=[] and isinstance (m,list) and m!=[]:
+            return self.multi_aux(n,m,[],[],[],0,0)
+        else: return "error"
+
+    def multi_aux(self,n,m,resultado,matriz,fila,indicefila,indicecolumna):
+        if indicefila==len(n):
+            return matriz*resultado
+        elif inidcecolumna==len(m[indicefila]):
+            return self.multi_aux(n,m,resultado,matriz+[fila],[],indicefila+1,0)
+        elif indicefila==0 or indicefila==(n-1):
+            
+        
