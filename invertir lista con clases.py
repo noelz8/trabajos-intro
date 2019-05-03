0602fda486122c24375d5e7e3d346fa3c6eb2db5
noelz8 <48070788+noelz8@users.noreply.github.com>
2019-05-03 14:03:04 -0600

    Add files via upload

--- invertir lista con clases.py
+++ invertir lista con clases.py
@@ -11,5 +11,20 @@ class Inv(object):
             return lista
         else:
             lista[inicio],lista[final]=lista[final],lista[inicio]
+            #temp = lista[final]
+            #lista[final]=lista[inicio]
+            #lista[inicio]= temp
             return self.invertir_aux(lista,inicio+1,final-1)
+#con solo 1 indice
+    def invertir2(self,lista):
+        if isinstance (lista,list) and lista !=[] and len(lista)%2==0:
+            return self.invertir_aux(lista,0)
+        else: return "Error"
+
+    def invertir2_aux(self,lista,inicio):
+        if inicio==len(lista)//2:
+            return lista
+        else:
+            lista[-(inicio+1)],lista[incio]=lista[inicio],lista[-(inicio+1)]
+            return self.invertir_aux(lista,inicio+1)
      
