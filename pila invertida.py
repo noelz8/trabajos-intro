83112ddb62007f511ff24966053dccd90aabf632
noelz8 <48070788+noelz8@users.noreply.github.com>
2019-05-24 14:55:07 -0600

    Add files via upload

---
+++ pila invertida.py
@@ -0,0 +1,55 @@
+class Pila:
+    def __init__(self):
+        self.__pi= [ ]
+    def len(self):
+        return len(self.__pi)
+    def is_empty(self):
+        return len(self. __pi)==0
+    def push(self,e):
+        self. __pi.append(e)
+    def top(self):
+        if self.is_empty( ):
+            raise MiGatito("Stack is empty")
+        else:return self. __pi[-1]
+    def pop(self):
+        if self.is_empty( ):
+            raise MiGatito("Stack is empty")
+        else: return self. __pi.pop( )
+    def test(self):
+        try:
+            self.stack=Pila()
+            self.stack.push(1)
+            self.stack.push(2)
+            self.stack.push(3)
+            self.stack.push(4)
+            self.stack.push(5)
+            self.stack.push(6)
+            self.stack.push(7)
+            self.stack.push(8)
+            while self.stack.is_empty() ==False:
+                print (self.stack.pop(),end=" ")
+            print()
+            #print("Nùmero de elementos en la pila: ",self.stack.len())
+            #print("Elelemento en la parte superior de la pila:", self.stack.top())
+            #print("Saca elemento",self.stack.pop())
+            #print("Saca elemento:", self.stack.pop())
+            #x=5
+            #y=0
+            #c= x/y
+        except MiGatito as e:
+            print("Ocurrio un error al sacar un elemento de una pila vacia.", str(type(e)), e.message())
+        except ZeroDivisionError as d:
+            print("Ocurrio un error al dividir.")
+        else:
+            print("Resultado")
+        finally:
+            print("Finally")
+class MiGatito(Exception):
+    def __init__(self,message):
+        self.message=message
+class DivisionCero(ZeroDivisionError):
+    def __init__(self,message):
+        self.message= message
+
+
+  
